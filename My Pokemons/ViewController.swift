c1a2f4bad9d358ed2a69e74fd1eb6ae24bd335d2
somabc <somabc@gmail.com>
2016-11-30 20:49:01 +0000

    Pikachu!

--- My Pokemons/ViewController.swift
+++ My Pokemons/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tbPokemonName: UITextField!
+    @IBOutlet weak var imgPokemon: UIImageView!
+    @IBOutlet weak var lbPokemonName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func bSearch(_ sender: AnyObject) {
+        lbPokemonName.text = tbPokemonName.text
+    }
 
 }
 
